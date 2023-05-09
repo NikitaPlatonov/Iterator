4e06996087bb8e2f14f7fb365ef7a9e9f6ce8f9c
Nikita Platonov <fifaakkaynt92@gmail.com>
2023-05-09 11:40:16 +0300

    corrected

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -1,5 +1,13 @@
-package ru.netology;public class Main {
+package ru.netology;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        for (int randomNumber : new Randoms(10, 100)) {
+            if (randomNumber == 100) {
+                System.out.println("Выпало число 100, на этом можно закончить");
+                break;
+            }
+            System.out.println("Выпало число " + randomNumber);
+        }
     }
 }
--- src/main/java/ru/netology/Randoms.java
+++ src/main/java/ru/netology/Randoms.java
@@ -1,2 +1,33 @@
-package ru.netology;public class Randoms {
+package ru.netology;
+
+import java.util.Iterator;
+import java.util.Random;
+
+public class Randoms implements Iterable<Integer> {
+    protected Random random;
+    protected int min;
+    protected int max;
+
+    public Randoms(int min, int max) {
+        random = new Random();
+        this.max = max + 1;
+        this.min = min + 1;
+
+        iterator().next();
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Iterator<Integer>() {
+            @Override
+            public boolean hasNext() {
+                return true;
+            }
+
+            @Override
+            public Integer next() {
+                return random.nextInt(max - min) + min;
+            }
+        };
+    }
 }
